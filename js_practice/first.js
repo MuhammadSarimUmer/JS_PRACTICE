0bbd86a2a8323ba1c01eaac2e07f6f8bec444a3c
MuhammadSarimUmer <muhammadsarim426426@gmail.com>
2025-06-08 01:54:10 +0500

    Initial commit

---
+++ js_practice/first.js
@@ -0,0 +1,32 @@
+console.log("hello world");
+
+let number = 100
+let String = "one hundred"
+
+console.log(number +" "+ String);
+
+let number2 = 20
+let sum = number + number2
+console.log(sum);
+let a = typeof(sum)
+console.log(a);
+
+let student  = {
+    name : "sarim",
+    age : 19,
+    city : "karachi",
+    isStudent : true,
+}
+console.log(student);
+
+const teacher = {
+    name : "Muhammad",
+    age : 42,
+    isTeacher : true,
+    city : "islamabad",
+}
+console.log(teacher);
+console.log(teacher.age);
+
+teacher.age = teacher.age + 5;
+console.log(teacher.age);
