7a33234017228bcafef2906d9cc28a0a30724a33
MuhammadSarimUmer <muhammadsarim426426@gmail.com>
2025-06-15 01:42:20 +0500

    second commit

---
+++ js_practice/second.js
@@ -0,0 +1,36 @@
+//these are comments(single line)
+/* thee are comments (multi line) */
+
+let a = 5;
+let b = 3;
+
+console.log("addition will be ",a+b);
+console.log("subtraction will be ",a-b);
+console.log("multiplication will be ",a*b);
+console.log("division will be ",a/b);
+console.log("modulus will be ",a%b);
+console.log("exp will be",a**b)
+console.log("increment will be",a++);
+console.log("decrement will be",a--);
+
+console.log(a+=2);
+console.log(a-=2);
+console.log(a*=2);
+console.log(a/=2);
+console.log(a%=2);
+console.log(a**=2);
+
+console.log(a==b);
+console.log(a!=b);
+console.log(a===b);
+console.log(a!==b);
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+
+let c = "5.2";
+
+console.log(c==a);
+console.log(c===a);
+
+
