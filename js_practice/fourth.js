499e3c9d77ff11163ed3ac38d526cf50ee53b66a
MuhammadSarimUmer <muhammadsarim426426@gmail.com>
2025-06-15 23:59:43 +0500

    fourth commit

---
+++ js_practice/fourth.js
@@ -0,0 +1,25 @@
+// let marks = [87,88,98,65,45,88,88];
+// size = marks.length;
+
+// for (let i = 0;i<size;i++){
+//     console.log(marks[i]);
+// }
+// console.log(typeof(marks));
+// console.log(marks.toString());
+// marks[2] = 76;
+// console.log(marks[2]);
+
+// let marks = [88,67,90,77,73];
+// let size = marks.length;
+// let average = 0;
+// let total = 0;
+// let totalSub = 0;
+// for(let i =0;i<size;i++){
+//     total += marks[i];
+//     totalSub ++;
+// }
+// average = total/totalSub;
+// console.log(average);
+
+let marks = [88,67,90,77,73];
+console.log(marks.slice(0,3));
